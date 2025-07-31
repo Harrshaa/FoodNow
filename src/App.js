850e15930d24c8a52496b47e4b5bdb0207c67f17
Sri Harsha Mutakodoor <harrshaa@Sris-MacBook-Air.local>
2025-07-31 10:18:12 +0530

    search , filter & Routings created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React, {useEffect, useState} from "react";
 import HeaderComponent from "./components/Header";
 import Body from "./components/Body";
+import About from './components/About';
+import ReactDom from "react-dom/client";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+
+import { createBrowserRouter,Outlet,RouterProvider } from "react-router-dom";
 
 const App =()=>{
     return(
         <div>
         <HeaderComponent/>
-        <Body/>
+        <Outlet/>
         </div>
     )
 }
 
-export default App ;
+const appRouter = createBrowserRouter(
+    [
+        {
+            path:"/",
+            element: <App/>,
+            children:[
+                {
+                    path:"/",
+                    element:<Body/>,
+                    
+                },   
+                {
+                    path:"/contact",
+                    element: <Contact/>
+                },
+                {
+                    path:"/about",
+                    element: <About/>
+                }
+
+            ],
+            errorElement: <Error/>
+
+        },
+   
+     
+    ]
+)
+
+
+
+const root= ReactDom.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={appRouter}/>);
+
+
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+import About from "./About";
 
 const HeaderComponent=()=>{
-    let btnName = "Login";
+    console.log("Header Called");
+    
     const [btn,setBtn]=useState("Login")
+    useEffect(()=>{
+        console.log("use effect called");
+
+    },)
     return(
         <div className="header">
             <div >
@@ -11,12 +18,13 @@ const HeaderComponent=()=>{
             </div>
             <div className="nav-item">
                 <ul className="navs">
-                    <li className="items">Home</li>
-                    <li className="items">About us</li>
-                    <li className="items">Contact</li>
+                    <li className="items"><Link to="/">Home</Link></li>
+                    <li className="items"><Link to="/about">About</Link></li>
+                    <li className="items"><Link to="/contact">Contact</Link></li>
                     <li className="items">Cart</li>
                     <button className="login-btn" onClick={()=>{
-                        setBtn("Logout")
+                        if(btn === "login") setBtn("logout");
+                        else setBtn("login");
                     }
                     }>{btn}</button>
 
